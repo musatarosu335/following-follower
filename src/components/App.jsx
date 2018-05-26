9e014ce0ee334a0b710f4604ff8043b0a29416c9
Kentaro Sakae <musamusa0607@gmail.com>
2018-05-26 18:17:18 +0900

    react-routerによるページ遷移テスト

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // import SignUp from './SignUp';
 import Login from './Login';
+import Users from './Users';
 
 const App = () => (
-  <Login />
+  <Router>
+    <Switch>
+      <Route
+        exact
+        path="/"
+        render={() => <Login />}
+      />
+      <Route
+        exact
+        path="/users"
+        render={() => <Users />}
+      />
+    </Switch>
+  </Router>
 );
 
 export default App;
---
+++ src/components/Users.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Users = () => (
+  <h1>This is Users Page</h1>
+);
+
+export default Users;
