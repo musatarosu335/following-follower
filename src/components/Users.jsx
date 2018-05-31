e9845278ff016b9b1b8a89a59c5c6f807a455db1
Kentaro Sakae <musamusa0607@gmail.com>
2018-05-31 22:53:12 +0900

    ユーザー一覧の表示

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,11 +1,40 @@
+import firebase from 'firebase/app';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+    };
+  }
+
+  componentWillMount() {
+    const db = firebase.firestore();
+    const users = [];
+
+    db.collection('users').get().then((snapshot) => {
+      snapshot.forEach((doc) => {
+        const { name } = doc.data();
+        users.push(name);
+      });
+      this.setState({
+        users,
+      });
+    });
+  }
+
   render() {
     return (
       <div>
-        <h1>Users Page</h1>
+        <h1>Users</h1>
+        <ul>
+          {this.state.users.map((user, i) => (
+            // eslint-disable-next-line
+            <li key={i}>{user}</li>
+          ))}
+        </ul>
         <Link to="/">Main</Link>
       </div>
     );
