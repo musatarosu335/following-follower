180ac2cebbfc4cbef74bcbae195b7237400c2f08
Kentaro Sakae <musamusa0607@gmail.com>
2018-06-02 16:35:12 +0900

    フォローしたユーザをfirestoreに保存

--- src/components/FollowButton.jsx
+++ src/components/FollowButton.jsx
@@ -1,9 +1,20 @@
+import firebase from 'firebase/app';
 import React from 'react';
 import PropTypes from 'prop-types';
 
 const handleClick = (userId) => {
   // ここでFirestoreに書き込む
-  console.log(userId);
+  const db = firebase.firestore();
+  const { currentUser } = firebase.auth();
+
+  db.collection(`users/${currentUser.uid}/following`).add({
+    uid: userId,
+    follow_time: new Date(),
+  }).then((docRef) => {
+    console.log('Document written with ID: ', docRef.id); // eslint-disable-line no-console
+  }).catch((err) => {
+    console.log(err); // eslint-disable-line no-console
+  });
 };
 
 const FollowButton = ({ userId }) => (
